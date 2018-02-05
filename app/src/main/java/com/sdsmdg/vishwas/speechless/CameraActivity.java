470a620b082991b88ff83d01ec661687c77b1b66
vishwas78 <vishwas.mittal78@gmail.com>
2018-02-05 07:56:40 +0530

    refactoring and fixed camera orientation problem

--- app/src/main/java/com/sdsmdg/vishwas/speechless/CameraActivity.java
+++ app/src/main/java/com/sdsmdg/vishwas/speechless/CameraActivity.java
@@ -1,19 +1,11 @@
 package com.sdsmdg.vishwas.speechless;
 
 import android.hardware.Camera;
-import android.media.CamcorderProfile;
 import android.media.MediaRecorder;
-import android.net.Uri;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.FrameLayout;
-
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class CameraActivity extends AppCompatActivity {
 
@@ -23,14 +15,6 @@ public class CameraActivity extends AppCompatActivity {
     private CameraPreview mPreview;
     private MediaRecorder mMediaRecorder;
 
-//    private boolean isRecording = false;
-//    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
-//        @Override
-//        public void onPictureTaken(byte[] data, Camera camera) {
-//            Log.e(TAG, "Picture taken and destroyed");
-//        }
-//    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,45 +22,12 @@ public class CameraActivity extends AppCompatActivity {
         Log.e(TAG, "onCreate called and now getting a camera instance");
 
         mCamera = getCameraInstance();
-        Log.e(TAG, "creating camera preview ibject");
+        Log.e(TAG, "creating camera preview object");
         mPreview = new CameraPreview(this, mCamera);
         FrameLayout preview = findViewById(R.id.camera_preview);
         Log.e(TAG, "adding preview object to layout");
         preview.addView(mPreview);
 
-        // Add a listener to the Capture button
-//        Button captureButton = findViewById(R.id.button_capture);
-//        captureButton.setOnClickListener(
-//                new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View v) {
-//                        Log.e(TAG, "onClicking capture button");
-//                        if (isRecording) {
-//                            // stop recording and release camera
-//                            mMediaRecorder.stop();  // stop the recording
-//                            releaseMediaRecorder(); // release the MediaRecorder object
-//                            mCamera.lock();         // take camera access back from MediaRecorder
-//                            // inform the user that recording has stopped
-////                            setCaptureButtonText("Capture");
-//                            isRecording = false;
-//                        } else {
-//                            // initialize video camera
-//                            if (prepareVideoRecorder()) {
-//                                // Camera is available and unlocked, MediaRecorder is prepared,
-//                                // now you can start recording
-//                                mMediaRecorder.start();
-//                                // inform the user that recording has started
-////                                setCaptureButtonText("Stop");
-//                                isRecording = true;
-//                            } else {
-//                                // prepare didn't work, release the camera
-//                                releaseMediaRecorder();
-//                                // inform user
-//                            }
-//                        }
-//                    }
-//                }
-//        );
     }
 
     public static Camera getCameraInstance() {
@@ -103,45 +54,6 @@ public class CameraActivity extends AppCompatActivity {
         releaseCamera();              // release the camera immediately on pause event
     }
 
-//    private boolean prepareVideoRecorder() {
-//        Log.e(TAG, "Preparing video recorder");
-//
-////        mCamera = getCameraInstance();
-//        mMediaRecorder = new MediaRecorder();
-//
-//        // Step 1: Unlock and set camera to MediaRecorder
-//        mCamera.unlock();
-//        mMediaRecorder.setCamera(mCamera);
-//
-//        // Step 2: Set sources
-//        mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
-//        mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
-//
-//        // Step 3: Set a CamcorderProfile (requires API Level 8 or higher)
-//        mMediaRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH));
-//
-////        Step 4: Set output file
-//        mMediaRecorder.setOutputFile(getOutputMediaFile(MEDIA_TYPE_VIDEO).toString());
-//
-//        // Step 5: Set the preview output
-//        mMediaRecorder.setPreviewDisplay(mPreview.getHolder().getSurface());
-//
-//
-//        // Step 6: Prepare configured MediaRecorder
-//        try {
-//            mMediaRecorder.prepare();
-//        } catch (IllegalStateException e) {
-//            Log.d(TAG, "IllegalStateException preparing MediaRecorder: " + e.getMessage());
-//            releaseMediaRecorder();
-//            return false;
-//        } catch (IOException e) {
-//            Log.d(TAG, "IOException preparing MediaRecorder: " + e.getMessage());
-//            releaseMediaRecorder();
-//            return false;
-//        }
-//        return true;
-//    }
-
     private void releaseMediaRecorder() {
         if (mMediaRecorder != null) {
             mMediaRecorder.reset();   // clear recorder configuration
@@ -157,50 +69,4 @@ public class CameraActivity extends AppCompatActivity {
             mCamera = null;
         }
     }
-
-
-    public static final int MEDIA_TYPE_IMAGE = 1;
-    public static final int MEDIA_TYPE_VIDEO = 2;
-
-    /**
-     * Create a file Uri for saving an image or video
-     */
-//    private static Uri getOutputMediaFileUri(int type) {
-//        return Uri.fromFile(getOutputMediaFile(type));
-//    }
-
-    /**
-     * Create a File for saving an image or video
-     */
-//    private static File getOutputMediaFile(int type) {
-//        // To be safe, you should check that the SDCard is mounted
-//        // using Environment.getExternalStorageState() before doing this.
-//
-//        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
-//                Environment.DIRECTORY_PICTURES), "MyCameraApp");
-//        // This location works best if you want the created images to be shared
-//        // between applications and persist after your app has been uninstalled.
-//
-//        // Create the storage directory if it does not exist
-//        if (!mediaStorageDir.exists()) {
-//            if (!mediaStorageDir.mkdirs()) {
-//                Log.d("MyCameraApp", "failed to create directory");
-//                return null;
-//            }
-//        }
-//
-//        // Create a media file name
-//        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-//        File mediaFile;
-//        if (type == MEDIA_TYPE_IMAGE) {
-//            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
-//                    "IMG_" + timeStamp + ".jpg");
-//        } else if (type == MEDIA_TYPE_VIDEO) {
-//            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
-//                    "VID_" + timeStamp + ".mp4");
-//        } else {
-//            return null;
-//        }
-//        return mediaFile;
-//    }
 }
--- app/src/main/java/com/sdsmdg/vishwas/speechless/CameraPreview.java
+++ app/src/main/java/com/sdsmdg/vishwas/speechless/CameraPreview.java
@@ -1,11 +1,14 @@
 package com.sdsmdg.vishwas.speechless;
 
 import android.content.Context;
-import android.graphics.Bitmap;
+import android.content.res.Configuration;
 import android.hardware.Camera;
 import android.util.Log;
+import android.view.Display;
+import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.WindowManager;
 
 import java.io.IOException;
 
@@ -17,23 +20,19 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
     private static final String TAG = "CameraPreview";
     private SurfaceHolder mHolder;
     private Camera mCamera;
+    private Context mContext;
 
     public CameraPreview(Context context, Camera camera) {
         super(context);
-        Log.e(TAG, "CameraPreview Constructor is called");
+        mContext = context;
         mCamera = camera;
 
-        // Install a SurfaceHolder.Callback so we get notified when the
-        // underlying surface is created and destroyed.
         mHolder = getHolder();
         mHolder.addCallback(this);
-        // deprecated setting, but required on Android versions prior to 3.0
         mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
     }
 
     public void surfaceCreated(SurfaceHolder holder) {
-        Log.e(TAG, "Surface Created");
-        // The Surface has been created, now tell the camera where to draw the preview.
         try {
             mCamera.setPreviewDisplay(holder);
             mCamera.startPreview();
@@ -43,31 +42,57 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
     }
 
     public void surfaceDestroyed(SurfaceHolder holder) {
-        Log.e(TAG, "surface destroyed");
-        // empty. Take care of releasing the Camera preview in your activity.
+        // Surface destroyed
     }
 
     public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
-        Log.e(TAG, "Surface changed");
-        // If your preview can change or rotate, take care of those events here.
-        // Make sure to stop the preview before resizing or reformatting it.
-
-        if (mHolder.getSurface() == null){
-            // preview surface does not exist
+        if (mHolder.getSurface() == null) {
             return;
         }
-
-        // stop preview before making changes
         try {
             mCamera.stopPreview();
-        } catch (Exception e){
-            // ignore: tried to stop a non-existent preview
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
         // set preview size and make any resize, rotate or
         // reformatting changes here
 
-        // start preview with new settings
+        Camera.Parameters parameters = mCamera.getParameters();
+
+        android.hardware.Camera.CameraInfo camInfo =
+                new android.hardware.Camera.CameraInfo();
+        android.hardware.Camera.getCameraInfo(getBackFacingCameraId(), camInfo);
+
+
+        Display display = ((WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+        int rotation = display.getRotation();
+        int degrees = 0;
+        switch (rotation) {
+            case Surface.ROTATION_0:
+                degrees = 0;
+                break;
+            case Surface.ROTATION_90:
+                degrees = 90;
+                break;
+            case Surface.ROTATION_180:
+                degrees = 180;
+                break;
+            case Surface.ROTATION_270:
+                degrees = 270;
+                break;
+        }
+
+        int result;
+        if (camInfo.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            result = (camInfo.orientation + degrees) % 360;
+            result = (360 - result) % 360;  // compensate the mirror
+        } else {  // back-facing
+            result = (camInfo.orientation - degrees + 360) % 360;
+        }
+        mCamera.setDisplayOrientation(result);
+
+
         try {
             mCamera.setPreviewCallback(new Camera.PreviewCallback() {
                 @Override
@@ -78,8 +103,27 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
             mCamera.setPreviewDisplay(mHolder);
             mCamera.startPreview();
 
-        } catch (Exception e){
+        } catch (Exception e) {
             Log.d(TAG, "Error starting camera preview: " + e.getMessage());
         }
     }
+
+
+    private int getBackFacingCameraId() {
+        int cameraId = -1;
+        // Search for the front facing camera
+        int numberOfCameras = Camera.getNumberOfCameras();
+        for (int i = 0; i < numberOfCameras; i++) {
+            Camera.CameraInfo info = new Camera.CameraInfo();
+            Camera.getCameraInfo(i, info);
+            if (info.facing == Camera.CameraInfo.CAMERA_FACING_BACK) {
+
+                cameraId = i;
+                break;
+            }
+        }
+        return cameraId;
+    }
+
+
 }
